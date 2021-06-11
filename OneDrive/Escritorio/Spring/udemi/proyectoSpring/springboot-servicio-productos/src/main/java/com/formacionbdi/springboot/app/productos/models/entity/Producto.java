3af0db3fa287843605e01e9cda3f414634fd92a5
Laura Cordoba <lauracor55@gmail.com>
2021-06-11 12:47:36 -0500

    version1 del microservicio

---
+++ OneDrive/Escritorio/Spring/udemi/proyectoSpring/springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
@@ -0,0 +1,67 @@
+package com.formacionbdi.springboot.app.productos.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+
+	private double precio;
+
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date create_At;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public Date getCreate_At() {
+		return create_At;
+	}
+
+	public void setCreate_At(Date create_At) {
+		this.create_At = create_At;
+	}
+
+	private static final long serialVersionUID = 1L;
+}
---
+++ OneDrive/Escritorio/Spring/udemi/proyectoSpring/springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into productos (nombre, precio, create_at) values('Panasonic', 800, NOW());
+insert into productos (nombre, precio, create_at) values('Challenger', 800, NOW());
+insert into productos (nombre, precio, create_at) values('Samsung', 800, NOW());
+insert into productos (nombre, precio, create_at) values('SONY', 800, NOW());
+insert into productos (nombre, precio, create_at) values('LG', 800, NOW());
